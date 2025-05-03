6fd87ad974c145238f371a953659a29be66327fd
Hillary Sousa <hillaryhpf@gmail.com>
2025-05-03 01:23:25 -0300

    adding CORS config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ initializeEnv();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:4000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
